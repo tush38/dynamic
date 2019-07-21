8f29a4545178ef23cf79db5b6475124e5feab128
Tushar <tusharguptat635@gmail.com>
2019-07-22 01:21:33 +0530

    Corrected my foolish mistake

--- index.js
+++ index.js
@@ -2,42 +2,43 @@ const http = require("http");
 const fs = require("fs");
 var el = fs.readFileSync("data.json");
 var jsondata = JSON.parse(el);
+// console.log(jsondata)
 var arr = [];
 var p = "";
 var out = "";
+var pb = fs.readFileSync("template_product.html");
+p = p + pb;
+console.log(p);
+var cards = fs.readFileSync("template_card.html");
+var over = fs.readFileSync("template_overview.html");
 const server = http
   .createServer(function(req, res) {
     var pat = req.url;
+    res.writeHead(200, { "content-type": "text/html" });
     arr = pat.split("?");
     var path = arr[0];
     if (path === "/products") {
-      var pb = fs.readFileSync("template_product.html");
-      p = p + pb;
-      var op = "";
+      var op = '';
       var inp = arr[1];
       var arr2 = inp.split("");
       var daal = arr2[3];
-      op += p
-        .replace(/{%image%}/g, jsondata[daal]["image"])
-        .replace(/{%productName%}/g, jsondata[daal]["productName"])
-        .replace(/{%from%}/g, jsondata[daal]["from"])
-        .replace(/{%nutrients%}/g, jsondata[daal]["nutrients"])
-        .replace(/{%quantity%}/g, jsondata[daal]["quantity"])
-        .replace(/{%price%}/g, jsondata[daal]["price"])
-        .replace(/{%description%}/g, jsondata[daal]["description"]);
+      op= p.replace(/{%image%}/g, jsondata[daal]["image"]);
+        op=op.replace(/{%productName%}/g, jsondata[daal]["productName"]);
+      op = op.replace(/{%from%}/g, jsondata[daal]["from"]);
+      op = op.replace(/{%nutrients%}/g, jsondata[daal]["nutrients"]);
+      op = op.replace(/{%quantity%}/g, jsondata[daal]["quantity"]);
+      op = op.replace(/{%price%}/g, jsondata[daal]["price"]);
+      op = op.replace(/{%description%}/g, jsondata[daal]["description"]);
       if (jsondata[daal]["organic"] === false) {
         op = op.replace(/{%NOT-ORGANIC%}/g, "not-organic");
       }
-
-      res.writeHead(200, { "content-type": "text/html" });
-      res.write(op);
+      console.log(op);
+      res.end(op);
     } else if (path === "/" || path === "/overview" || path === "/home") {
       var c = "";
       var ov = "";
       var cdata = "";
-      var over = fs.readFileSync("template_overview.html");
       ov += over;
-      var cards = fs.readFileSync("template_card.html");
       c += cards;
       for (var i = 0; i < jsondata.length; i++) {
         out = c
@@ -52,9 +53,7 @@ const server = http
         cdata += out;
       }
       var out2 = ov.replace(/{%CARD-TEMPLATE%}/g, cdata);
-
-      res.writeHead(200, { "content-type": "text/html" });
-      res.write(out2);
+      res.end(out2);
     } else if (path === "/api") {
       res.write(el);
 
@@ -64,5 +63,5 @@ const server = http
     }
   });
   var port = process.env.PORT||80;
-  server.listen(port);
-console.log("Server has started on port "+port);
+  server.listen(3000);
+// console.log("Server has started on port "+port);
